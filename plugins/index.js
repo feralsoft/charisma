bcbc42faf047e8a69b07361713ed06452e607c07
MarcelRusu <marcelrusu@protonmail.com>
2024-02-03 10:29:10 -0500

    folder restructure

---
+++ plugins/index.js
@@ -0,0 +1,13 @@
+import copy_paste from "./copy-paste/copy-paste.coil";
+import drag_and_drop from "./drag-and-drop/drag-and-drop.coil";
+import highlight_elements_who_match_query from "./highlight-elements-who-match-query/highlight-elements-who-match-query.coil";
+import toggle_representation from "./toggle-representation/toggle-representation.coil";
+import replace_numeric_with_slider from "./replace-numeric-with-slider/replace-numeric-with-slider.coil";
+
+export default {
+  copy_paste,
+  drag_and_drop,
+  highlight_elements_who_match_query,
+  toggle_representation,
+  replace_numeric_with_slider,
+};
