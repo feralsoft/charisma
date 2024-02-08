288c0389db0c5535a5b04a4e29c06348e87b37e0
MarcelRusu <marcelrusu@protonmail.com>
2024-02-08 16:44:53 -0500

    - wip persistence strategy - wip generator based parser

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import compile from "@coil-lang/compiler/";
 import { defineConfig } from "vite";
+import fs from "fs/promises";
+import * as prettier from "prettier";
 const fileRegex = /\.(coil)$/;
-
 function coil() {
   return {
     name: "coil-lang",
@@ -16,7 +17,28 @@ function coil() {
   };
 }
 
+function updateIndexCss() {
+  return {
+    name: "index-css",
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === "/update_css") {
+          let body = "";
+          req.on("data", (b) => (body += b.toString()));
+          req.on("end", async () => {
+            let { css, file } = JSON.parse(body);
+            css = await prettier.format(css, { semi: false, parser: "css" });
+            fs.writeFile(file, css);
+            next();
+          });
+        }
+        next();
+      });
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [coil()],
+  plugins: [coil(), updateIndexCss()],
   build: { target: "esnext" },
 });
