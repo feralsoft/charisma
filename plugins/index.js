62411caf5ca3ac978d479dc85621c4a49108c003
MarcelRusu <marcelrusu@protonmail.com>
2024-02-12 15:34:03 -0500

    new plugin - add-new-rule

--- plugins/index.js
+++ plugins/index.js
@@ -3,6 +3,7 @@ import drag_and_drop from "./drag-and-drop/drag-and-drop.coil";
 import highlight_elements_who_match_query from "./highlight-elements-who-match-query/highlight-elements-who-match-query.coil";
 import toggle_representation from "./toggle-representation/toggle-representation.coil";
 import replace_numeric_with_slider from "./replace-numeric-with-slider/replace-numeric-with-slider.coil";
+import add_new_rule from "./add-new-rule/add-new-rule.coil";
 
 export default {
   copy_paste,
@@ -10,4 +11,5 @@ export default {
   highlight_elements_who_match_query,
   toggle_representation,
   replace_numeric_with_slider,
+  add_new_rule,
 };
