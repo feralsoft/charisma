ce773fc9ba3da0a941f7c4b68dc45820cc6efd6c
MarcelRusu <marcelrusu@protonmail.com>
2024-02-12 16:38:23 -0500

    plugin to be able to delete nodes

--- plugins/index.js
+++ plugins/index.js
@@ -4,6 +4,7 @@ import highlight_elements_who_match_query from "./highlight-elements-who-match-q
 import toggle_representation from "./toggle-representation/toggle-representation.coil";
 import replace_numeric_with_slider from "./replace-numeric-with-slider/replace-numeric-with-slider.coil";
 import add_new_rule from "./add-new-rule/add-new-rule.coil";
+import delete_node from "./delete-node/delete-node.coil";
 
 export default {
   copy_paste,
@@ -12,4 +13,5 @@ export default {
   toggle_representation,
   replace_numeric_with_slider,
   add_new_rule,
+  delete_node,
 };
