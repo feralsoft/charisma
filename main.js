5f18306468ae84ce02faeb0122e3ff521f01dd65
MarcelRusu <marcelrusu@protonmail.com>
2024-02-13 17:39:46 -0500

    error handling for adding new rules

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
+window.try__b = function (lambda) {
+  try {
+    return lambda();
+  } catch (e) {
+    if (!(e instanceof Error)) e = new Error(e);
+    return e;
+  }
+};
+
 import "./index.css";
 import "./main.coil";
