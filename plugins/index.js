4872ffefe355de4803186b5c7bfdb9712c4e4e42
MarcelRusu <marcelrusu@protonmail.com>
2024-02-14 16:50:53 -0500

    remove highlight elements plugin

--- plugins/index.js
+++ plugins/index.js
@@ -1,6 +1,5 @@
 import copy_paste from "./copy-paste/copy-paste.coil";
 import drag_and_drop from "./drag-and-drop/drag-and-drop.coil";
-import highlight_elements_who_match_query from "./highlight-elements-who-match-query/highlight-elements-who-match-query.coil";
 import toggle_representation from "./toggle-representation/toggle-representation.coil";
 import replace_numeric_with_slider from "./replace-numeric-with-slider/replace-numeric-with-slider.coil";
 import add_new_rule from "./add-new-rule/add-new-rule.coil";
@@ -10,7 +9,6 @@ import focus from "./focus/focus.coil";
 export default {
   copy_paste,
   drag_and_drop,
-  highlight_elements_who_match_query,
   toggle_representation,
   replace_numeric_with_slider,
   add_new_rule,
