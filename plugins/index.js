fd655d815cdcb2a1a576635aa3a6f0e6ea16dd63
MarcelRusu <marcelrusu@protonmail.com>
2024-03-04 16:45:10 -0500

    experiment with ordering rules by type

--- plugins/index.js
+++ plugins/index.js
@@ -9,6 +9,7 @@ import size_preview from "./size-preview/size-preview.coil";
 import color_preview from "./color-preview/color-preview.coil";
 import flex_preview from "./flex-preview/flex-preview.coil";
 import update_text from "./update-text/update-text.coil";
+import order_rules from "./order-rules/order-rules.coil";
 
 export default {
   copy_paste,
@@ -22,4 +23,5 @@ export default {
   color_preview,
   flex_preview,
   update_text,
+  order_rules,
 };
