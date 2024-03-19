77ab892edbe2d02349ee543bd190b039c94e2f87
MarcelRusu <marcelrusu@protonmail.com>
2024-03-19 12:49:33 -0400

    plugins are now aware of which rule they belong in

--- domx.tsx
+++ domx.tsx
@@ -1,22 +1,23 @@
-declare let render: any;
 declare let get: any;
 declare let set: any;
+type CSSRule = any;
 declare let remove: any;
 declare let React: any;
 
 export default {
   name: "flex-preview",
+  // query to search for `display: flex`
   query:
     "[data-kind=property]:has(> [data-attr=name] [data-value=display], > [data-attr=expr] [data-value=flex])",
   insert_after: (
     <div
       class="flex-direction-preview"
       data-flex-preview={get("flex-direction")}
-      on:click={function* () {
-        if (yield get("flex-direction") === "column") {
-          yield remove("flex-direction");
+      on:click={function* (self: CSSRule) {
+        if (yield self.get("flex-direction") === "column") {
+          yield self.remove("flex-direction");
         } else {
-          yield set("flex-direction", "column");
+          yield self.set("flex-direction", "column");
         }
       }}
     />
