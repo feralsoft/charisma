511e6988033927741235419db4b5dcd981894491
MarcelRusu <marcelrusu@protonmail.com>
2024-03-26 16:45:59 -0400

    icon

--- domx.tsx
+++ domx.tsx
@@ -7,8 +7,10 @@ declare let React: any;
 export default {
   name: "flex-preview",
   // query to search for `display: flex`
-  query:
-    "[data-kind=property]:has(> [data-attr=name] [data-value=display], > [data-attr=expr] [data-value=flex])",
+  query: `[data-kind=property]:has(
+    > [data-attr=name] [data-value=display],
+    > [data-attr=expr] [data-value=flex]
+  )`,
   insert_after: (
     <div
       class="flex-direction-preview"
