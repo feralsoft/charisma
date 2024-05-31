d31449dd1b3027df9a608943c5e70a00a486bb92
MarcelRusu <marcelrusu@protonmail.com>
2024-05-31 12:41:05 -0400

    setup rocket server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,33 +1,24 @@
+#[macro_use]
+extern crate rocket;
 use html::Render;
+use rocket::http::ContentType;
 
 mod html;
 
-fn main() {
-    let result = biome_css_parser::parse_css(
-        ".test { --color: 20%; }",
-        biome_css_parser::CssParserOptions::default(),
-    );
-    let output = result.tree().render_html();
+#[get("/")]
+fn index() -> (ContentType, String) {
+    (
+        ContentType::HTML,
+        biome_css_parser::parse_css(
+            ".btn { font-size: 20px; }",
+            biome_css_parser::CssParserOptions::default(),
+        )
+        .tree()
+        .render_html(),
+    )
+}
 
-    let expected_output = "<div data-kind=\"rule\"><div data-attr=\"selector\">".to_owned()
-        + "<div data-kind=\"class\">"
-        + &html::render_value("test".to_string())
-        + "</div>"
-        + "</div>"
-        + "<div data-attr=\"properties\">"
-        + "<div data-kind=\"property\" data-property-kind=\"variable\">"
-        + "<div data-attr=\"name\">"
-        + &html::render_value("--color".to_owned())
-        + "</div>"
-        + "<div data-attr=\"value\">"
-        + "<div data-kind=\"unit\" data-unit-type=\"percentage\">"
-        + &html::render_value("20".to_owned())
-        + "</div>"
-        + "</div>"
-        + "</div>"
-        + "</div>"
-        + "</div>";
-    // println!("{}", expected_output);
-
-    assert!(expected_output == output)
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
