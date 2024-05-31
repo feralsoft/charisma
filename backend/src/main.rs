b730a1e87251cdfe5d7acbcde23198b6ac487eef
MarcelRusu <marcelrusu@protonmail.com>
2024-05-31 12:59:33 -0400

    incorporate styles

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,31 @@
 #[macro_use]
 extern crate rocket;
+
 use html::Render;
-use rocket::http::ContentType;
+use rocket::{http::ContentType, tokio::fs::read_to_string};
 
 mod html;
 
+async fn css() -> String {
+    read_to_string("src/index.css").await.unwrap()
+}
+
 #[get("/")]
-fn index() -> (ContentType, String) {
+async fn index() -> (ContentType, String) {
+    let code_html = biome_css_parser::parse_css(
+        ".btn { font-size: 20px; }",
+        biome_css_parser::CssParserOptions::default(),
+    )
+    .tree()
+    .render_html();
     (
         ContentType::HTML,
-        biome_css_parser::parse_css(
-            ".btn { font-size: 20px; }",
-            biome_css_parser::CssParserOptions::default(),
-        )
-        .tree()
-        .render_html(),
+        format!(
+            "<style>{}</style>
+            <div class=\"--editor\" spellcheck=\"false\">{}</div>",
+            css().await,
+            code_html
+        ),
     )
 }
 
