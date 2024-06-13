f8957b547220b2aafc46f0380ee8cbee6d3ba966
MarcelRusu <marcelrusu@protonmail.com>
2024-06-13 14:34:22 -0400

    fix self link to go up selector parent path

--- src/js/explore_siblings.js
+++ src/js/explore_siblings.js
@@ -1,10 +1,9 @@
 const ALL_SELECTOR_PARTS_QUERY =
   '[data-kind="rule"] > [data-attr="selector"] [data-value]';
 
-function siblings_root(should_show, options) {
+function siblings_dropdown(options) {
   let elem = document.createElement("div");
   elem.classList.add("siblings-root");
-  elem.dataset.show = should_show;
   elem.append(...options);
   return elem;
 }
@@ -21,40 +20,47 @@ function remove_all_dropdowns() {
   for (let dropdown of document.querySelectorAll(".siblings-root"))
     dropdown.remove();
   for (let self_link of document.querySelectorAll(".self-link"))
-    self_link.replaceWith(self_link.querySelector("[data-kind]"));
+    self_link.replaceWith(self_link.querySelector(":scope > [data-kind]"));
 }
 
-function find_idx_for(part) {
+function find_idx_for(search_part) {
   let idx = 1;
-  for (let p of document.querySelectorAll(ALL_SELECTOR_PARTS_QUERY)) {
-    if (p === part) return idx;
+  for (let iter_part of document.querySelectorAll(ALL_SELECTOR_PARTS_QUERY)) {
+    if (iter_part === search_part) return idx;
     idx++;
   }
   throw new Error("can't find part :(");
 }
 
+function create_self_link_for(leaf_node, idx) {
+  let self_link = document.createElement("a");
+  self_link.classList.add("self-link");
+  self_link.href = `${location.pathname}/${idx}`;
+  leaf_node.insertAdjacentElement("afterend", self_link);
+  self_link.append(leaf_node);
+}
+
 document.addEventListener("DOMContentLoaded", (_) => {
   for (let part of document.querySelectorAll(ALL_SELECTOR_PARTS_QUERY)) {
     part.addEventListener("mousedown", async (_) => {
+      // if we are in an existing dropdown don't rebuild
       let existing_dropdown_for_self = part
         .closest("[data-kind]")
         .querySelector(":scope > .siblings-root");
-      remove_all_dropdowns();
       if (existing_dropdown_for_self) return;
+      // remove old dropdowns
+      remove_all_dropdowns();
+      // find the selector idx of current part
       let idx = find_idx_for(part);
       let leaf_node = part.closest("[data-kind]");
       let siblings = await fetch(`${location.pathname}/${idx}/siblings`).then(
         (r) => r.json(),
       );
-      // ahh this is kind of hacky, but I'm just replacing the part with a link... :| it works though
-      let self_link = document.createElement("a");
-      self_link.classList.add("self-link");
-      self_link.href = `${location.pathname}/${idx}`;
-      leaf_node.insertAdjacentElement("afterend", self_link);
-      self_link.append(leaf_node);
+      // turn myself into a link
+      create_self_link_for(leaf_node, idx);
+      // create dropdown with sibling links
       leaf_node.append(
-        siblings_root(
-          true,
+        siblings_dropdown(
           siblings.map((path) => {
             let [_, elem] = path.at(idx - 1);
             return option(path.map(([part, _]) => part).join(""), elem);
