9c99f7592ba5bdc452408caf492b9ae3f27628c8
MarcelRusu <marcelrusu@protonmail.com>
2024-06-20 18:34:25 -0400

    fix memory leak with toggle_property

--- src/js/toggle_property.js
+++ src/js/toggle_property.js
@@ -1,23 +1,24 @@
+async function toggle() {
+  let property = this.closest('[data-kind="property"]');
+  let name = property.querySelector("[data-attr=name]").textContent;
+  let is_commented = property.dataset.commented === "true";
+  let action = is_commented ? "enable" : "disable";
+  let editor = property.closest(".--editor");
+  await fetch(url_for(editor, `/${name}/${action}`), { method: "POST" });
+  property.dispatchEvent(new Event("reload", { bubbles: true }));
+}
+
 function insert_comment_button(src) {
   let button = document.createElement("button");
   button.innerHTML = "<div class='text'></div>";
   button.classList.add("toggle-comment");
-  button.addEventListener("mousedown", async (_) => {
-    let name = src.querySelector("[data-attr=name]").textContent;
-    let is_commented =
-      src.closest('[data-kind="property"]').dataset.commented === "true";
-    let action = is_commented ? "enable" : "disable";
-    let editor = src.closest(".--editor");
-    await fetch(url_for(editor, `/${name}/${action}`), { method: "POST" });
-    src.dispatchEvent(new Event("reload", { bubbles: true }));
-  });
+  button.addEventListener("mousedown", toggle);
   src.prepend(button);
 }
 
 const PROPERTY_SELECTOR = "[data-attr=properties] > [data-kind=property]";
 
 function init(editor) {
-  editor.addEventListener("loaded", (_) => init(editor));
   for (let property of editor.querySelectorAll(PROPERTY_SELECTOR))
     insert_comment_button(property);
 }
