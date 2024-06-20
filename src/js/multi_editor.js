b37d14278b0cdcff23df2f763a3f7f951569ce8d
MarcelRusu <marcelrusu@protonmail.com>
2024-06-20 17:01:13 -0400

    clean up multi_editor

--- src/js/multi_editor.js
+++ src/js/multi_editor.js
@@ -1,51 +1,54 @@
+let reloading = new Map();
+
+async function reload(editor, base_url) {
+  // if (reloading.get(editor)) return;
+  // reloading.set(editor, true);
+  // for (let editor_ of document.querySelectorAll(".--editor"))
+  //   editor_.dispatchEvent(new Event("reload"));
+  let url = new URL(base_url);
+  url.pathname += "/rule";
+  let new_rule = await fetch(url).then((r) => r.text());
+  editor.innerHTML = new_rule;
+  editor.dataset.url = url;
+  catch_links(editor);
+  editor.dispatchEvent(new Event("loaded"));
+  // reloading.set(editor, false);
+}
+
 function catch_links(editor) {
   for (let a of editor.querySelectorAll("a")) {
     a.addEventListener("click", async (e) => {
       e.preventDefault();
-      let url = new URL(a.href);
-      url.pathname += "/rule";
-      let new_rule = await fetch(url).then((r) => r.text());
-      editor.innerHTML = new_rule;
-      editor.dataset.url = a.href;
-      editor.dispatchEvent(new Event("loaded"));
+      reload(editor, a.href);
     });
   }
 }
-let reloading = new Map();
+
+function x_offset() {
+  return Number(
+    document.body.style.getPropertyValue("--x-offset").split("px")[0],
+  );
+}
+
+function y_offset() {
+  return Number(
+    document.body.style.getPropertyValue("--y-offset").split("px")[0],
+  );
+}
+
 function init(editor) {
   catch_links(editor);
   let { width, height } = document.body.getBoundingClientRect();
   let { width: editor_width, height: editor_height } =
     editor.getBoundingClientRect();
-  let x_offset = Number(
-      document.body.style.getPropertyValue("--x-offset").split("px")[0],
-    ),
-    y_offset = Number(
-      document.body.style.getPropertyValue("--y-offset").split("px")[0],
-    );
-  editor.style.left = `${width / 2 - editor_width / 2 - x_offset}px`;
-  editor.style.top = `${height / 2 - editor_height / 2 - y_offset}px`;
+  editor.style.left = `${width / 2 - editor_width / 2 - x_offset()}px`;
+  editor.style.top = `${height / 2 - editor_height / 2 - y_offset()}px`;
   snap_editor(editor);
-  editor.addEventListener("reload", async (_) => {
-    if (reloading.get(editor)) return;
-    reloading.set(editor, true);
-    for (let editor_ of document.querySelectorAll(".--editor")) {
-      editor_.dispatchEvent(new Event("reload"));
-    }
-
-    let new_rule = await fetch(url_for(editor, "/rule")).then((r) => r.text());
-    editor.innerHTML = new_rule;
-    editor.dataset.url = url_for(editor);
-    catch_links(editor);
-    editor.dispatchEvent(new Event("loaded"));
-    reloading.set(editor, false);
-  });
+  editor.addEventListener("reload", (_) => reload(editor, url_for(editor)));
 }
 
 document.addEventListener("DOMContentLoaded", (_) => {
-  for (let editor of document.querySelectorAll(".--editor")) {
-    init(editor);
-  }
+  for (let editor of document.querySelectorAll(".--editor")) init(editor);
   let canvas = document.querySelector(".canvas");
   canvas.addEventListener("new-editor", (_) => {
     let editor = document.querySelector(".--editor:last-child");
