edc95dfd6486309c726185103860eaead2c3277c
MarcelRusu <marcelrusu@protonmail.com>
2024-06-26 14:07:19 -0400

    focus property should be double click

--- src/js/focus.js
+++ src/js/focus.js
@@ -1,17 +1,27 @@
-const FOCUSABLE = [".--editor", '[data-kind="property"]'];
-
 window.addEventListener("mousedown", (e) => {
   if (!e.isTrusted) return; // don't trust simulated mouse clicks
   for (let focused of document.querySelectorAll(".focused"))
     focused.classList.remove("focused");
   let elements = document.elementsFromPoint(e.clientX, e.clientY);
 
-  for (let query of FOCUSABLE) {
-    for (let elem of elements) {
-      let result = elem.closest(query);
-      if (!result) continue;
-      result.classList.add("focused");
-      break;
-    }
+  for (let elem of elements) {
+    let result = elem.closest(".--editor");
+    if (!result) continue;
+    result.classList.add("focused");
+    break;
+  }
+});
+
+window.addEventListener("dblclick", (e) => {
+  if (!e.isTrusted) return; // don't trust simulated mouse clicks
+  for (let focused of document.querySelectorAll(".focused"))
+    focused.classList.remove("focused");
+  let elements = document.elementsFromPoint(e.clientX, e.clientY);
+
+  for (let elem of elements) {
+    let result = elem.closest('[data-kind="property"]');
+    if (!result) continue;
+    result.classList.add("focused");
+    break;
   }
 });
