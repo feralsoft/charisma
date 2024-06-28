b8fe350133592849f1b83907d88484a054b1bef2
MarcelRusu <marcelrusu@protonmail.com>
2024-06-28 13:44:19 -0400

    impl ctrl+shift+tab

--- src/js/tab-cycle.js
+++ src/js/tab-cycle.js
@@ -5,10 +5,18 @@ window.addEventListener("keydown", (e) => {
       .closest(".--editor");
     if (!current_editor) return;
     e.preventDefault();
-    let next_editor = current_editor.nextElementSibling;
-    if (!next_editor?.classList?.contains("--editor"))
-      next_editor = document.querySelector(".--editor"); // first
 
-    next_editor.dispatchEvent(new Event("tab-into", { bubbles: true }));
+    let tabbed_into_editor;
+    if (e.shiftKey) {
+      tabbed_into_editor = current_editor.previousElementSibling;
+      if (!tabbed_into_editor?.classList?.contains("--editor"))
+        tabbed_into_editor = document.querySelector(".--editor:last-child"); // last
+    } else {
+      tabbed_into_editor = current_editor.nextElementSibling;
+      if (!tabbed_into_editor?.classList?.contains("--editor"))
+        tabbed_into_editor = document.querySelector(".--editor:first-child"); // first
+    }
+
+    tabbed_into_editor.dispatchEvent(new Event("tab-into", { bubbles: true }));
   }
 });
