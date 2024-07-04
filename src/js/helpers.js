0c34cb7504168929c1042ab1bdb94cb65fa360d3
MarcelRusu <marcelrusu@protonmail.com>
2024-07-04 16:37:16 -0400

    better drop detection, using custom overlap algo that loops through all editor groups

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -4,9 +4,21 @@ export function px_var(elem, name, fallback = 0) {
   return Number(value.split("px")[0]);
 }
 
+export function num_var(elem, name, fallback = 0) {
+  let value = elem.style.getPropertyValue(name);
+  if (!value) return fallback;
+  return Number(value);
+}
+
 export function find_map(iterable, fn) {
   for (let elem of iterable) {
     let result = fn(elem);
     if (result) return result;
+  }
+}
+
+export function find(iterable, fn) {
+  for (let elem of iterable) {
+    if (fn(elem)) return elem;
   }
 }
--- src/js/multi-editor.js
+++ src/js/multi-editor.js
@@ -1,4 +1,4 @@
-import { find_map } from "./helpers.js";
+import { find, num_var } from "./helpers.js";
 
 const { invoke } = window.__TAURI__.tauri;
 
@@ -44,10 +44,51 @@ function new_group(position) {
   return group;
 }
 
+function is_overlapping(group, editor, editor_position) {
+  let { width: editor_width, height: editor_height } =
+    editor.getBoundingClientRect();
+  let editor_top = editor_position.y;
+  let editor_left = editor_position.x;
+  let editor_right = editor_left + editor_width;
+  let editor_bottom = editor_top + editor_height;
+
+  let { width: group_width, height: group_height } =
+    group.getBoundingClientRect();
+  let group_left = num_var(group, "--x");
+  let group_top = num_var(group, "--y");
+  let group_bottom = group_top + group_height;
+  let group_right = group_left + group_width;
+
+  return (
+    group_bottom > editor_top &&
+    editor_bottom > group_top &&
+    editor_left < group_right &&
+    editor_right > group_left
+  );
+}
+
 function put_in_group(editor, position) {
+  // position is top-left
+  //
+  // and we search for --editor-group with that position
+  // but this fails easily in this case
+  //
+  // |----------------|
+  // | dropped-editor |
+  // |  |----------------|
+  // |  | old  editor    |
+  // |  |----------------|
+  // |----------------|
+  //
+  // ^ here I dropped an editor over-top of an existing editor `old editor`
+  // but since top left isn't within old editor, it won't snap..
+  //
+  // intersection observer requires you to register all the elements to watch afaik
+  // this seems hard to maintain, so for now we will just loop over all elements & check overlap
+
   let group =
-    find_map(document.elementsFromPoint(position.x, position.y), (elem) =>
-      elem.closest(".--editor-group"),
+    find(document.querySelectorAll(".--editor-group"), (group) =>
+      is_overlapping(group, editor, position),
     ) ?? new_group(position);
 
   group.append(editor);
@@ -97,25 +138,13 @@ function init(editor) {
   editor.addEventListener("reload", (_) => reload(editor));
 }
 
-function on_intersection(entries, observer) {
-  console.log(entries, observer);
-}
-
-let observer = new IntersectionObserver(on_intersection, {
-  root: document.body,
-  rootMargin: "0px",
-  threshold: 1.0,
-});
-
 document.addEventListener("DOMContentLoaded", (_) => {
   let canvas = document.querySelector(".canvas");
   canvas.addEventListener("new-editor", ({ detail: editor }) => {
     init(editor);
-    observer.observe(editor);
     editor.addEventListener("drag-finished", ({ detail: { position } }) =>
       snap_and_group(editor, position),
     );
-
     editor.addEventListener("blur", (_) => snap_size(editor));
     editor.addEventListener("focus", (_) => snap_size(editor));
   });
