a30da6fba474a74f788d1b3b25f49e42a1d0d25a
MarcelRusu <marcelrusu@protonmail.com>
2024-07-22 16:32:50 -0400

    click preview to bring rule back on screen

--- src/js/off-screen.js
+++ src/js/off-screen.js
@@ -1,12 +1,30 @@
-import { px_var } from "./helpers.js";
+import { num_var, px_var } from "./helpers.js";
 import { h } from "./html.js";
 
 let x_offset, y_offset;
+
+const GRID_SIZE = 25;
 
 function existing_offscreen_for(editor) {
   return document.querySelector(
     `.offscreen-editor-preview[data-selector="${editor.dataset.selector.replaceAll('"', '\\"')}"]`,
   );
+}
+
+function bring_editor_onscreen(editor) {
+  let { top, left, bottom, right } = editor.getBoundingClientRect();
+  let { width, height } = document.body.getBoundingClientRect();
+
+  let x_offset = px_var(document.body, "--x-offset"),
+    y_offset = px_var(document.body, "--y-offset");
+
+  if (top < 0) y_offset -= top - (GRID_SIZE + 24);
+  if (left < 0) x_offset -= left - (GRID_SIZE + 2);
+  if (right > width) x_offset -= right - width + 12;
+  if (bottom > height) y_offset -= bottom - height + (GRID_SIZE + 2);
+
+  document.body.style.setProperty("--x-offset", `${x_offset}px`);
+  document.body.style.setProperty("--y-offset", `${y_offset}px`);
 }
 
 function preview_offscreen(editor, x, y, placements) {
@@ -22,6 +40,15 @@ function preview_offscreen(editor, x, y, placements) {
       "data-selector": editor.dataset.selector,
       "data-placement": placements.join(" "),
       style: { "--x": x, "--y": y },
+    });
+
+    preview.addEventListener("click", (e) => {
+      preview.remove();
+      bring_editor_onscreen(
+        document.querySelector(
+          `.--editor[data-selector="${editor.dataset.selector.replaceAll('"', '\\"')}"]`,
+        ),
+      );
     });
 
     canvas.append(preview);
