e98c5d0b1cd6d78c245bb3e504f2a41a810bccc5
MarcelRusu <marcelrusu@protonmail.com>
2024-07-27 19:00:10 -0400

    errors go away after successful load

--- src/js/display-error.js
+++ src/js/display-error.js
@@ -7,9 +7,15 @@ function handle_error({ detail: error }) {
   this.setAttribute("data-error-msg", msg);
 }
 
+function remove_error() {
+  this.removeAttribute("data-error-type");
+  this.removeAttribute("data-error-msg");
+}
+
 document.addEventListener("DOMContentLoaded", (_) => {
   let canvas = document.querySelector(".canvas");
   canvas.addEventListener("new-editor", ({ detail: { editor } }) => {
     editor.addEventListener("invoke-error", handle_error);
+    editor.addEventListener("loaded", remove_error);
   });
 });
