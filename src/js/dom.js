d5fa18b43618b16f2e572cbf518208355102d9af
MarcelRusu <marcelrusu@protonmail.com>
2024-07-30 00:16:12 -0400

    remove once

--- src/js/dom.js
+++
@@ -1,6 +0,0 @@
-export function once(elem, event_name, handler) {
-  elem.addEventListener(event_name, function self(e) {
-    handler(e);
-    elem.removeEventListener(event_name, self);
-  });
-}
--- src/js/invoke.js
+++ src/js/invoke.js
@@ -1,11 +1,11 @@
-import { once } from "./dom.js";
-
 const { invoke: tauri_invoke } = window.__TAURI__.tauri;
 
 export default async function invoke(editor, path, args) {
   try {
     let result = await tauri_invoke(path, args);
-    let loaded = new Promise((r) => once(editor, "loaded", r));
+    let loaded = new Promise((r) =>
+      editor.addEventListener("loaded", r, { once: true }),
+    );
     editor.dispatchEvent(new Event("reload"));
     await loaded;
     return result;
